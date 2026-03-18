a85e59a7d27808d7c640cdcbc503ec690bbf6f52
joelpfeiffer <joelpfeiffer15@gmail.com>
2026-03-18 05:39:31 +0100

    Update database.py

--- pipeline/database.py
+++ pipeline/database.py
@@ -1,4 +1,10 @@
 import sqlite3
+import sys
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+
 from app.config import DB_PATH
 
 def init_db():
