51c683197b73db25f736ffdf2f8c659f6e6805fc
joelpfeiffer <joelpfeiffer15@gmail.com>
2026-03-18 05:23:24 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,7 @@
 import os
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 DB_PATH = os.path.join(BASE_DIR, "data", "pension.db")
+
 URL = "https://www.zwitserleven.nl/fondsen"
