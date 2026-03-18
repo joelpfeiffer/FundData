a03540e9c02eda91b0c99a4f225377abe7bfc6b3
joelpfeiffer <joelpfeiffer15@gmail.com>
2026-03-18 04:51:44 +0100

    Update api.py

--- api/api.py
+++ api/api.py
@@ -1,1 +1,13 @@
+from fastapi import FastAPI
+import sqlite3
+import pandas as pd
+from app.config import DB_PATH
 
+app = FastAPI()
+
+@app.get("/prices")
+def get_prices():
+    conn = sqlite3.connect(DB_PATH)
+    df = pd.read_sql("SELECT * FROM prices", conn)
+    conn.close()
+    return df.to_dict(orient="records")
