53796d8d02f5f0f75fa67cc57634e5b274ca7846
joelpfeiffer <joelpfeiffer15@gmail.com>
2026-06-19 13:18:26 +0000

    update

--- app/dashboard.py
+++ app/dashboard.py
@@ -3876,8 +3876,11 @@ if tab10 is not None:
                         valuation_df[
                             valuation_df[
                                 "valuation_date"
-                            ]
-                            == valuation_date
+                            ].dt.date
+                            ==
+                            pd.to_datetime(
+                                valuation_date
+                            ).date()
                         ]
                     )
 
