4c4be2a40eb4b90f49c73faa6838ff5f9d2ba5e2
joelpfeiffer <joelpfeiffer15@gmail.com>
2026-06-19 06:12:02 +0000

    UPDATE

--- app/dashboard.py
+++ app/dashboard.py
@@ -3187,7 +3187,7 @@ if tab10 is not None:
                     )
 
                     total_cost = sum(
-                        s["cost_total"]
+                        s["costS_total"]
                         or 0
                         for s in snapshots.data
                     )
