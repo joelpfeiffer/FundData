48cd885b90b9ed818eca3ae41f06f251aa101f63
joelpfeiffer <joelpfeiffer15@gmail.com>
2026-06-24 15:55:33 +0000

    update

--- app/dashboard.py
+++ app/dashboard.py
@@ -368,6 +368,7 @@ if is_admin and is_logged_in:
     tab9 = tabs[8]
     tab10 = tabs[9]
     tab11 = tabs[10]
+    Tab12 = tabs[11]
 
 elif is_admin:
 
@@ -377,7 +378,7 @@ elif is_logged_in:
 
     tab9 = tabs[7]
     tab10 = tabs[8]
-    tab12 = tabs[11]
+    tab12 = tabs[9]
 
 # =========================
 # OVERVIEW
